840c880d9760c01e2a669f24f35551bca4564b3b
Worome <41673409+Worome@users.noreply.github.com>
2019-08-16 14:07:33 +0200

    Primeros pasos

--- app/src/main/java/com/trianacodes/script/listview/MainActivity.java
+++ app/src/main/java/com/trianacodes/script/listview/MainActivity.java
@@ -2,12 +2,39 @@ package com.trianacodes.script.listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tv1;
+    private ListView lv1;
+
+    private String nombres[]={"Samuel", "Valentina", "Santiago", "Alejandro", "Valeria", "Benjamín",
+            "Gerardo", "Carlos", "David", "Sofía"};
+    private String edades [] = {"18", "25", "32", "17", "24", "20", "27", "15", "19", "23"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv1 = findViewById(R.id.tv1);
+        lv1 = findViewById(R.id.lv1);
+
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, R.layout.list_item_personalizado, nombres);
+        lv1.setAdapter(adaptador);
+        //Llamo al método setOnItemClickListener para saber qué item se ha seleccionado.
+        lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
+
+
     }
 }
